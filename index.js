c5ebbc0e48bd05467324b6726ea48dc41715297d
Yogesh Jamatia <jmtygsh@gmail.com>
2025-08-15 15:43:30 +0530

    backend done

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
+import { serve } from "inngest/express";
 import userRoutes from "./routes/user";
+import ticketRoutes from "./routes/ticket";
+import { inngest } from "./inngest/client";
+import { onSignUp } from "./inngest/functions/on-signup";
+import { onTicketCreated } from "./inngest/functions/on-ticket-create";
+
+import dotenv from "dotenv";
+dotenv.config();
 
 const port = process.env.PORT || 3000;
 const app = express();
@@ -9,6 +17,14 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use("api/auth", userRoutes);
+app.use("api/tickets", ticketRoutes);
+app.use(
+  "/api/inngest",
+  serve({
+    client: inngest,
+    functions: [onSignUp, onTicketCreated],
+  })
+);
 
 mongoose
   .connect(process.env.MONGO_URL)
--- routes/ticket.js
+++ routes/ticket.js
@@ -1,5 +1,11 @@
 import express from "express";
+import { authenticate } from "../middlewares/auth";
+import { getTickets, getTicket, createTicket } from "../controllers/ticket";
 
 const router = express.Router();
+
+router.get("/", authenticate, getTickets);
+router.get("/:id", authenticate, getTicket);
+router.post("/", authenticate, createTicket);
 
 export default router;
